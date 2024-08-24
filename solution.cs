3414491b1e9e75ff679927f68627be5c86b750fb
Kooreis <kooreis.official@gmail.com>
2024-08-24 02:56:06 -0500

    In this update, the main function is defined. It initializes the values and weights of the items, the maximum weight the knapsack can hold, and the number of items.

--- solution.cs
+++ solution.cs
@@ -1,4 +1,3 @@
-```C#
 using System;
 
 class Program
@@ -9,27 +8,4 @@ class Program
         int[] wt = new int[] { 10, 20, 30 };
         int W = 50;
         int n = val.Length;
-        Console.WriteLine(KnapSack(W, wt, val, n));
     }
-
-    static int KnapSack(int W, int[] wt, int[] val, int n)
-    {
-        int[,] K = new int[n + 1, W + 1];
-
-        for (int i = 0; i <= n; i++)
-        {
-            for (int w = 0; w <= W; w++)
-            {
-                if (i == 0 || w == 0)
-                    K[i, w] = 0;
-                else if (wt[i - 1] <= w)
-                    K[i, w] = Math.Max(val[i - 1] + K[i - 1, w - wt[i - 1]], K[i - 1, w]);
-                else
-                    K[i, w] = K[i - 1, w];
-            }
-        }
-
-        return K[n, W];
-    }
-}
-```
