28847c6f66571e6934748933bdef7cf1639cd3f3
Kooreis <kooreis.official@gmail.com>
2024-08-25 02:56:06 -0500

    In this update, the user is prompted to enter the values of the items. A for loop is used to store each value in the 'values' array.

--- solution.java
+++ solution.java
@@ -1,9 +1,4 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number of items: ");
-        int n = scanner.nextInt();
-        int[] values = new int[n];
-        int[] weights = new int[n];
+System.out.println("Enter the values of the items: ");
+        for (int i = 0; i < n; i++) {
+            values[i] = scanner.nextInt();
+        }
